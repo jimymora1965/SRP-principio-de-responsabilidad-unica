a9ae02543a9c3a57379927596b5eb111c6ff0286
Jimy Mora <jimymora@gmail.com>
2023-09-22 12:27:32 -0500

    first commit

---
+++ srp.py
@@ -0,0 +1,49 @@
+
+# principio de responsabilidad unica: cada clase o funcion respondiendo por su actividad
+print("Principio de responsabilidad unica Single responsibility principle")
+
+class TanqueDeCombustible:
+    def __init__(self):
+        self.combustible = 100
+        
+    def agregar_combustible(self, cantidad):
+        self.combustible = self.combustible + cantidad
+        
+    def obtener_combustible(self):
+        return self.combustible
+    
+    def usar_combustible(self, cantidad):
+        self.combustible = self.combustible - cantidad
+        
+class Auto:
+    def __init__(self, tanque):
+        self. posicion = 0
+        self.tanque = tanque
+        
+    def mover(self, distancia):
+        if self.tanque.obtener_combustible() >= distancia / 2:
+            self.posicion = self.posicion + distancia
+            self.tanque.usar_combustible(distancia/2)
+            print("Has movido el auto exitosamente")
+        else:
+            print("No hay suficiente combustible")
+            
+    def obtener_posicion(self):
+        return self.posicion
+    
+
+        
+tanque = TanqueDeCombustible()
+carro = Auto(tanque)
+
+print(carro.obtener_posicion())
+carro.mover(10)
+print(carro.obtener_posicion())
+carro.mover(20)
+print(carro.obtener_posicion())
+carro.mover(60)
+print(carro.obtener_posicion())
+carro.mover(100)
+print(carro.obtener_posicion())
+carro.mover(100)
+print(carro.obtener_posicion())
